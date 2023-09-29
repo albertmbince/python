a7cbc2113d61dfb348c5d835b8d0aa9dca5d6656
Albert M Bince <133676636+albertmbince@users.noreply.github.com>
2023-09-29 17:57:17 +0530

    Add files via upload

--- function.py
+++ function.py
@@ -33,14 +33,127 @@
 # print(special())   
 
 
-def square():
-    list=[]
-    list1=[]
-    for i in range(0,4):
-        a=int(input("enter number="))
-        list.append(a)
-    for i in list:
-        i=i**2
-        list1.append(i)
-    d=print("the squred list is ",list1)
-square()
+# def square():
+#     list=[]
+#     list1=[]
+#     for i in range(0,4):
+#         a=int(input("enter number="))
+#         list.append(a)
+#     for i in list:
+#         i=i**2
+#         list1.append(i)
+#     d=print("the squared list is ",list1)
+# square()
+
+
+# def list():
+#   l=[]
+#   c=0
+#   for i in range(0,3):
+#       n=int(input("enter number="))
+#       l.append(n)
+#   for i in l:
+#       b=i**2
+#       c=c+b
+#   print("the sum of squares=",c)
+
+      
+list()    
+
+
+# def list():
+#   l=[]
+#   l2=[]
+#   l3=[]
+# #   c=0
+#   for i in range(0,4):
+#       n=int(input("enter number="))
+#       l.append(n)
+#   print("the list one is inserted")
+#   for i in range(0,5):
+#       n=int(input("enter number="))
+#       l2.append(n)
+#   print("the list two is inserted")
+#   for i in l:
+#       if i indef prime():
+#     n=int((input("enter limit")))
+#     for i in range(0,n+1):
+#         if i%2!=0 or i==2:
+#             print(i)
+# prime() l2:
+#           l3.append(i)
+  
+#   print("The common numbers in both lists are=",l3)        
+      
+# list()
+
+# def factorial():
+#     c=1
+#     n=int(input("enter the number="))
+#     for i in range(1,n+1):
+#        c=c*i
+#     print(c)
+# factorial()
+
+# def prime():
+#     n=int((input("enter limit")))
+#     for i in range(0,n+1):
+#         if i%2!=0 or i==2:
+#             if i%3!=0:
+#               print(i)
+# prime()
+
+# def fibonacci():
+#  c=0
+#  d=0
+#  a=1
+#  n=int((input("enter the value=")))
+#  for i in range(1,n):
+#        print(d)
+#        c=d+a
+#        d=a
+#        a=c
+       
+# fibonacci()      
+
+
+# def pri():
+#       for i in range(65,68):
+#         for j in range(i,64,-1):
+#             print(chr(i),end=" ")
+#         print()
+# pri()    
+    
+    
+# def pri():
+#  n=70 
+#  k=65
+#  for i in range(65,n):
+#     for j in range(65,i+1,1):
+#         print(chr(k),end="  ")
+#         k=k+1
+#     print()
+# pri()    
+    
+# def pri():
+#  n=70 
+#  k=65
+#  for i in range(65,n):
+#     for j in range(65,i+1):
+#         print(chr(j),end="  ")
+#         k=k+1
+#     print()
+# pri()    
+
+
+
+def pri():
+ n=70 
+ k=65
+ for i in range(65,n):
+    for j in range(65,i+1):
+        print(chr(i),end="  ")
+        i=i-1
+    print()
+pri()    
+    
