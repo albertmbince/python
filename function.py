05027453b25ee369fd7068117baab456378e2710
Albert M Bince <133676636+albertmbince@users.noreply.github.com>
2023-10-04 17:55:14 +0530

    Add files via upload

--- function.py
+++ function.py
@@ -147,13 +147,149 @@ list()
 
 
 
-def pri():
- n=70 
- k=65
- for i in range(65,n):
-    for j in range(65,i+1):
-        print(chr(i),end="  ")
-        i=i-1
-    print()
-pri()    
+# def pri():
+#  n=70 
+#  k=65
+#  for i in range(65,n):
+#     for j in range(65,i+1):
+#         print(chr(i),end="  ")
+#         i=i-1
+#     print()
+# pri()    
+
+# def star():
+#     for i in range(5,0,-1):
+#         for j in range(0,i):
+#             print("",end=" ")
+#             # i=i+1
+#         print("*")
+#     for i in range(0,5):
+#               for j in range(0,i):
+#                  print("",end=" ")
+#             # i=i+1
+#               print("*")
+# star()
+
+
+# 
+# def A():
+#  n = 5
+#  for i in range(1, n+1):
+#     for j in range(n - i):
+#         print(' ', end='')
+
+#     for k in range(2 * i - 1):
+#         if k == 0 or k == 2 * i - 2:
+#             print('*', end='')
+#         else:
+#             if i == 3:
+#                 print('*', end='')
+#             else:
+#                 print(' ', end='')
+#     print()
+# A()
+# def pattern():
+#     k=1
+#     for i in range(1,8):
+                  
+#                  for j in range(0,i):
+#                       print("*",end="" )    
+#                  print()
+                        
+#                  if i%2==0 :  
+#                       for j in range(1,i+1):
+#                          print(j,end="")
+#                         
+#                       print()
+                 
+#                  else:
+#                     for j in range(i,0,-1):
+#                        print(j,end="")
+#                     print()    
+                     
+                     
+            
+                     
+# pattern()
+
+
+# def decimal():
+#  l=[]
+#  n=float(input("enter decimal number:"))
+#  a=n/2
+#  b=a%10
+#  l.append(b)
+#  print(l)
+ 
+   
+   
+
+# decimal()
+
+
+# def ascending():
+#    l=[]
+#    for i in range(0,6):
+#       n=int(input("enter number="))
+#       l.append(n)
+#    l.sort()
+#    print("ascending order is =",l)
+#    l.sort(reverse= True)
+#    print('descending order is=',l)
+# ascending()
+
+
+# def fibo():
+#     a=int(input("enter number:"))
+#     b=0
+#     c=1
+#     for i in range(0,a):
+#        if b<=a:
+#         print(b)
+#         d=b+c
+#         b=c
+#         c=d
+# fibo()
+
+# def sum():
+#     l=[3,8,12,7,6,10,21,15]
+#     total=18
+#     for i in l:
+#         for j in l:
+#             if i+j==total:
+#              print("the numbers that give sum 18 are",i,j)
+            
+           
+      
+# sum()
+# def words():
     
+    
+#     l=["apple","banana","cherry","date"]
+#     for x in l :
+#         for y in l:
+#             for j in x:
+#                 for q in y:
+                    
+#                   if j==q:
+#                    print(x,y)   
+                         
+# words()
+
+
+# def number():
+#     l=[2,3,4,5,6]
+#     print("the numbers which give product even are")
+#     for i in l:
+#         for j in l:
+#             a=i*j
+            
+#             if a%2==0:
+#                 print(i,j)
+#     print("the numbers which give sum odd are")
+#     for i in l:
+#      for j in l:
+#         b=i+j
+#         if b%2!=0:
+#                 print(i,j)
+# number()
